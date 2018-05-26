b1c92516f9be4f388c2b6a570371056197f1e35a
Tatiana Tkachenko <tatyana.tkachenko@nixsolutions.com>
2018-05-26 19:35:43 +0300

    Changed app name

--- ecosystem.dev.config.js
+++ ecosystem.dev.config.js
@@ -6,8 +6,8 @@ module.exports = {
       interpreter_args: '--inspect=9229',
       env: {
         NODE_ENV: 'development',
-        DEBUG: 'myapp:*',
-        APP_NAME: 'myapp:api'
+        DEBUG: 'takeItEasy:*',
+        APP_NAME: 'takeItEasy:api'
       },
       watch: './server',
       ignore_watch: ['node_modules', './public']
